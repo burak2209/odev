95c300108cf788b2cbe7dfb61c6117f05c7ed327
ediz32 <117947167+ediz32@users.noreply.github.com>
2022-11-11 11:42:11 +0300

    Create burak can yıılmaz1.dart

---
+++ burak can yıılmaz1.dart
@@ -0,0 +1,39 @@
+class Canlilar {
+    //canlilar islemleri bu blokta yapilacaktir
+
+    Canlilar(){
+        print("Canlilar nesnesi oluşturuldu.");
+    }
+
+///Fields
+static const  String Ad = 'Burak';
+int yas = 25 ;
+bool cinsiyeterkek=true, cinsiyetkadin=false ;
+
+
+
+}
+
+class Yuru {
+    Yuru();
+    print"(${this.ad} isimli canli yurur.")
+
+
+}
+
+class insan {
+      print("insan nesnesi olusturuldu.");
+
+///Fields
+static const String Soyad = "yılmaz";
+bool GozlukluMu = true, Gozludegil = false;
+int AylikGelir = 20000;
+static const int vize = 90;
+statik const int final = 85;
+
+
+
+}
+
+
+
